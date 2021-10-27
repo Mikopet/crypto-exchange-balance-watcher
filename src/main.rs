99b2025b1a41ffd2dff5c891e2bedfd1c2094b53
Peter Mikola <mikopet@gmail.com>
2021-10-27 11:23:34 +0200

    add some formatting to the balances, get rid of nulls

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,31 @@ use std::env;
 
 use binance::account::*;
 use binance::api::*;
+use binance::model::*;
 
-#[derive(Debug)]
+use std::fmt::{Display, Error, Formatter};
+
 pub struct Credentials {
     pub api_key: String,
     pub secret_key: String,
+}
+
+struct BalVec(Vec<Balance>);
+
+impl Display for BalVec {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        let mut line_separated = String::new();
+
+        for bal in &self.0 {
+            let sum: f32 = &bal.free.parse::<f32>().unwrap() + &bal.locked.parse::<f32>().unwrap();
+
+            line_separated.push_str(&format!("{:>7} |", &bal.asset));
+            line_separated.push_str(&format!("{:>12} free", &bal.free));
+            line_separated.push_str(&format!("{:>12} locked", &bal.locked));
+            line_separated.push_str(&format!(" | = {} \n", sum));
+        }
+        write!(f, "{}", line_separated)
+    }
 }
 
 fn main() {
@@ -20,13 +40,19 @@ fn main() {
         secret_key: env::var("BINANCE_SECRET_KEY").expect("Failed to read BINANCE_SECRET_KEY"),
     };
 
-    println!("{:?}", creds);
-
     let account: Account = Binance::new(Some(creds.api_key), Some(creds.secret_key));
 
     match account.get_account() {
-        Ok(answer) => println!("{:?}", answer.balances),
+        Ok(answer) => {
+            let balances = BalVec(
+                answer
+                    .balances
+                    .into_iter()
+                    .filter(|bal| bal.free.parse::<f32>().unwrap() > 0.)
+                    .collect(),
+            );
+            println!("{}", balances)
+        }
         Err(e) => println!("Error: {:?}", e),
     }
-
 }
