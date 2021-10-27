6b654ed363dbcfbb0d256c38a8bdbab758dabe46
Peter Mikola <mikopet@gmail.com>
2021-10-27 11:05:31 +0200

    add dotenv feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
+#[derive(Debug)]
+pub struct Credentials {
+    pub api_key: String,
+    pub secret_key: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let creds = Credentials {
+        api_key: env::var("BINANCE_API_KEY").expect("Failed to read BINANCE_API_KEY"),
+        secret_key: env::var("BINANCE_SECRET_KEY").expect("Failed to read BINANCE_SECRET_KEY"),
+    };
+
+    println!("{:?}", creds);
 }
